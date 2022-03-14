2a8e2c990a846395d93e31112fc76df82a100e3d
TicTacToe333 <aidencedwards@gmail.com>
2022-03-14 09:53:53 -0600

    Update stripped_platformer.py
    Added a shield, short range attack, and long range attack. Each character has their own color associated with their attacks. Added some comments for clarity. Deleted some redundant code

--- stripped_platformer.py
+++ stripped_platformer.py
@@ -10,7 +10,8 @@ from raylib.colors import (
 )
 
 
-
+player_1_color = RED
+player_2_color = BLACK
 # Initialization
 global g_evening_out, g_even_out_target
 g_evening_out = False
@@ -31,6 +32,14 @@ class Player:
         self.position = position
         self.speed = speed
         self.can_jump = can_jump
+        self._damage = 0
+        self._direction = "left"
+    
+    def get_direction(self):
+        return self._direction
+
+    def set_direction(self, direction):
+        self._direction = direction
 
 
 class EnvItem:
@@ -40,15 +49,44 @@ class EnvItem:
         self.blocking = blocking
         self.color = color
 
-def update_player(player, env_items, delta, keys):
+def update_player(player, env_items, delta, keys, player_color):
+    #left (a and j)
     if pyray.is_key_down(keys[0]):
         player.position.x -= PLAYER_HOR_SPD * delta
+        player.set_direction("left")
+
+    #right (d and l)
     if pyray.is_key_down(keys[1]):
         player.position.x += PLAYER_HOR_SPD * delta
+        player.set_direction("right")
+        
+    #jump (w and i)
     if pyray.is_key_down(keys[2]) and player.can_jump:
         player.speed = -PLAYER_JUMP_SPD
         player.can_jump = False
+    
+    #s and k (shield)
+    if pyray.is_key_down(keys[3]):
+        pyray.draw_rectangle_lines(int(player.position.x)-25, int(player.position.y)-45,50,50,player_color)
+        
+    #q and u (ranged attack)
+    if pyray.is_key_down(keys[4]):
+        if player.get_direction() == "left":
+            end_x = 0
+        else:
+            end_x = SCREEN_WIDTH
+        
+        pyray.draw_line(int(player.position.x), int(player.position.y) - 20, end_x, int(player.position.y) - 20, player_color)
+        
 
+    #e and o (punch attack)
+    if pyray.is_key_down(keys[5]):
+        if player.get_direction() == "left":
+            start_x = int(player.position.x) - 40
+        else:
+            start_x = int(player.position.x) + 20
+        pyray.draw_rectangle(start_x, int(player.position.y)-30,20,20,player_color)
+    
     hit_obstacle = False
     for ei in env_items:
         p = player.position
@@ -75,7 +113,8 @@ def update_player(player, env_items, delta, keys):
 player = Player(pyray.Vector2(400, 280), 0, False)
 player2 = Player(pyray.Vector2(400,280), 0, False)
 env_items = (
-    EnvItem(pyray.Rectangle(0, 0, 1000, 400), 0, LIGHTGRAY),
+    #I don't think this one does anything
+    #EnvItem(pyray.Rectangle(0, 0, 1000, 400), 0, LIGHTGRAY),
     EnvItem(pyray.Rectangle(0, 400, 1000, 200), 1, GRAY),
     EnvItem(pyray.Rectangle(300, 200, 400, 10), 1, GRAY),
     EnvItem(pyray.Rectangle(250, 300, 100, 10), 1, GRAY),
@@ -88,17 +127,19 @@ while not pyray.window_should_close():  # Detect window close button or ESC key
     # Update
     delta_time = pyray.get_frame_time()
 
-    keys_1 = [pyray.KEY_A, pyray.KEY_D, pyray.KEY_W]
-    keys_2 = [pyray.KEY_J, pyray.KEY_L, pyray.KEY_I]
-    update_player(player, env_items, delta_time, keys_1)
-    update_player(player2, env_items, delta_time, keys_2)
+    pyray.begin_drawing()
+    keys_1 = [pyray.KEY_A, pyray.KEY_D, pyray.KEY_W, pyray.KEY_S, pyray.KEY_Q, pyray.KEY_E]
+    keys_2 = [pyray.KEY_J, pyray.KEY_L, pyray.KEY_I, pyray.KEY_K, pyray.KEY_U, pyray.KEY_O]
+    update_player(player, env_items, delta_time, keys_1, player_1_color)
+    update_player(player2, env_items, delta_time, keys_2, player_2_color)
 
+   # player.direction = "right"
     #restarts the game
     if pyray.is_key_pressed(pyray.KEY_R):
         player.position = pyray.Vector2(400, 280)
+        player2.position = pyray.Vector2(400, 280)
 
     # Draw
-    pyray.begin_drawing()
     pyray.clear_background(LIGHTGRAY)
 
 
@@ -116,10 +157,10 @@ while not pyray.window_should_close():  # Detect window close button or ESC key
         int(player2.position.y) - 40,
         40, 40)
 
-    pyray.draw_rectangle_rec(player_rect, RED)
-    pyray.draw_rectangle_rec(player_rect2, BLACK)
+    pyray.draw_rectangle_rec(player_rect, player_1_color)
+    pyray.draw_rectangle_rec(player_rect2, player_2_color)
+  #  pyray.draw_rectangle_lines(400,280,10,10,RED)
 
-    pyray.end_mode_2d()
 
     pyray.end_drawing()
 
