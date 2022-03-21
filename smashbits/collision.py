fc6e30c3e310d730b41cf14260ee0bbf4cc9aa97
Tysonloveless <86091297+Tysonloveless@users.noreply.github.com>
2022-03-21 09:38:40 -0600

    collision class added and added the update collision into main

---
+++ smashbits/collision.py
@@ -0,0 +1,8 @@
+class Collision:
+
+    def __init__(self):
+        filller = 0
+
+    def check_collision(player, player2):
+        if player.position.x < player2.position.x - 1:
+            print("collision detected")
--- smashbits/main.py
+++ smashbits/main.py
@@ -3,6 +3,7 @@ from player import Player
 from envitem import EnvItem
 from update_player import update_player
 import pyray
+from collision import Collision
 
 from raylib.colors import (
     DARKGRAY,
@@ -15,77 +16,83 @@ from raylib.colors import (
 player_1_color = RED
 player_2_color = BLACK
 
-# Initialization
-G = 2000
-PLAYER_JUMP_SPD = 800.0
-PLAYER_HOR_SPD = 200.0
+def main():
+    # Initialization
+    G = 2000
+    PLAYER_JUMP_SPD = 800.0
+    PLAYER_HOR_SPD = 200.0
 
-SCREEN_WIDTH = 1600
-SCREEN_HEIGHT = 900
+    SCREEN_WIDTH = 1600
+    SCREEN_HEIGHT = 900
+    collision = Collision
 
-pyray.set_target_fps(60)  # Set our game to run at 60 frames-per-second
+    pyray.set_target_fps(60)  # Set our game to run at 60 frames-per-second
 
-pyray.init_window(SCREEN_WIDTH, SCREEN_HEIGHT,
-                  'Super Smash Blocks - Raylib')
+    pyray.init_window(SCREEN_WIDTH, SCREEN_HEIGHT,
+                    'Super Smash Blocks - Raylib')
 
-# Main intialization
-player = Player(pyray.Vector2(400, 280), 0, False)
-player2 = Player(pyray.Vector2(1200,280), 0, False)
-env_items = (
-    #I don't think this one does anything
-    #EnvItem(pyray.Rectangle(0, 0, 1000, 400), 0, LIGHTGRAY),
-    EnvItem(pyray.Rectangle(200, 600, 1200, 200), 1, GRAY),
-    EnvItem(pyray.Rectangle(550, 400, 500, 10), 1, GRAY),
-    EnvItem(pyray.Rectangle(450, 500, 300, 10), 1, GRAY),
-    EnvItem(pyray.Rectangle(850, 500, 300, 10), 1, GRAY),
-)
-
-
-while not pyray.window_should_close():  # Detect window close button or ESC key
-    # Update
-    delta_time = pyray.get_frame_time()
-
-    pyray.begin_drawing()
-    keys_1 = [pyray.KEY_A, pyray.KEY_D, pyray.KEY_W, pyray.KEY_S, pyray.KEY_Q, pyray.KEY_E]
-    keys_2 = [pyray.KEY_J, pyray.KEY_L, pyray.KEY_I, pyray.KEY_K, pyray.KEY_U, pyray.KEY_O]
-    update_player(player, env_items, delta_time, keys_1, player_1_color)
-    update_player(player2, env_items, delta_time, keys_2, player_2_color)
-
-   # player.direction = "right"
-    #restarts the game
-    if pyray.is_key_pressed(pyray.KEY_R):
-        player.position = pyray.Vector2(400, 280)
-        player2.position = pyray.Vector2(1200, 280)
-
-    # Draw
-    pyray.clear_background(LIGHTGRAY)
-
-    for env_item in env_items:
-        pyray.draw_rectangle_rec(env_item.rect, env_item.color)
-
-    player_rect = pyray.Rectangle(
-        int(player.position.x) - 20, 
-        int(player.position.y) - 40,
-        40, 40
+    # Main intialization
+    player = Player(pyray.Vector2(400, 280), 0, False)
+    player2 = Player(pyray.Vector2(1200,280), 0, False)
+    env_items = (
+        #I don't think this one does anything
+        #EnvItem(pyray.Rectangle(0, 0, 1000, 400), 0, LIGHTGRAY),
+        EnvItem(pyray.Rectangle(200, 600, 1200, 200), 1, GRAY),
+        EnvItem(pyray.Rectangle(550, 400, 500, 10), 1, GRAY),
+        EnvItem(pyray.Rectangle(450, 500, 300, 10), 1, GRAY),
+        EnvItem(pyray.Rectangle(850, 500, 300, 10), 1, GRAY),
     )
 
-    player_rect2 = pyray.Rectangle(
-        int(player2.position.x) - 20,
-        int(player2.position.y) - 40,
-        40, 40)
 
-    pyray.draw_rectangle_rec(player_rect, player_1_color)
-    pyray.draw_rectangle_rec(player_rect2, player_2_color)
-  #  pyray.draw_rectangle_lines(400,280,10,10,RED)
+    while not pyray.window_should_close():  # Detect window close button or ESC key
+        # Update
+        delta_time = pyray.get_frame_time()
 
-    pyray.end_drawing()
+        pyray.begin_drawing()
+        keys_1 = [pyray.KEY_A, pyray.KEY_D, pyray.KEY_W, pyray.KEY_S, pyray.KEY_Q, pyray.KEY_E]
+        keys_2 = [pyray.KEY_J, pyray.KEY_L, pyray.KEY_I, pyray.KEY_K, pyray.KEY_U, pyray.KEY_O]
+        update_player(player, env_items, delta_time, keys_1, player_1_color)
+        update_player(player2, env_items, delta_time, keys_2, player_2_color)
+        collision.check_collision(player, player2)
 
-# De-Initialization
-pyray.close_window()  # Close window and OpenGL context
+    # player.direction = "right"
+        #restarts the game
+        if pyray.is_key_pressed(pyray.KEY_R):
+            player.position = pyray.Vector2(400, 280)
+            player2.position = pyray.Vector2(1200, 280)
+
+        # Draw
+        pyray.clear_background(LIGHTGRAY)
+
+        for env_item in env_items:
+            pyray.draw_rectangle_rec(env_item.rect, env_item.color)
+
+        player_rect = pyray.Rectangle(
+            int(player.position.x) - 20, 
+            int(player.position.y) - 40,
+            40, 40
+        )
+
+        player_rect2 = pyray.Rectangle(
+            int(player2.position.x) - 20,
+            int(player2.position.y) - 40,
+            40, 40)
+
+        pyray.draw_rectangle_rec(player_rect, player_1_color)
+        pyray.draw_rectangle_rec(player_rect2, player_2_color)
+    #  pyray.draw_rectangle_lines(400,280,10,10,RED)
+
+        pyray.end_drawing()
+
+    # De-Initialization
+    pyray.close_window()  # Close window and OpenGL context
+
+
+main()
 
 #####    TO DO    #####
 #ATTACKS - damage, ranged attack bug when moving/ facing right
 #SHIELD - negate damage, timer/ limit movement
 #COLLISION DETECTION FOR OTHER PLAYER
-#SCREEN BOUNDS - edge death detection
+#SCREEN BOUNDS - edge death detection     - not an immediate priority
 #SEPARATE INTO CLASSES
