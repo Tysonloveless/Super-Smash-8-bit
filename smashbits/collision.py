2abcbaf15ce4900f6f9b3edd730669f428749542
Tysonloveless <86091297+Tysonloveless@users.noreply.github.com>
2022-03-21 14:33:50 -0600

    Update collision.py
    updated the collision file to detect when the players are touching.
    
    - bug exists where it will detect collision when player two is jumping over, but not touching, player one.
    
    this does not occur when player one jumps over player two

--- smashbits/collision.py
+++ smashbits/collision.py
@@ -1,8 +1,16 @@
+from pickle import FALSE
+
+
 class Collision:
 
     def __init__(self):
-        filller = 0
+        self.hit_obstacle = FALSE
 
     def check_collision(player, player2):
-        if player.position.x < player2.position.x - 1:
-            print("collision detected")
+        player_position = {'x': int(player.position.x) - 20, 'y':
+            int(player.position.y) - 40, 'width': 40, 'height': 40}
+        player2_position = {'x': int(player2.position.x) - 20, 'y':
+            int(player2.position.y) - 40, 'width':40, 'height': 40}
+
+        if player_position['x'] < player2_position['x'] + player2_position['width'] and player_position['x'] + player_position['width'] > player2_position['x'] and player_position['y'] + player2_position['height'] and player_position['y'] + player_position['height'] > player2_position['y']:
+            print('collision detected')
